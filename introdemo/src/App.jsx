d83b206989c6b1023b56d115ed8a94c1f364b0ec
Marlos Leles <marlosleles@gmail.com>
2025-01-10 19:50:22 +0000

    Creating component Hello and using it in the component App

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,3 +1,11 @@
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
 const App = () => {
   const now = new Date()
   const a = 10
@@ -5,8 +13,14 @@ const App = () => {
   console.log(now, a+b)
   return (
     <div>
+      <h1>Greetings</h1>
       <p>Hello World, it is {now.toString()}</p>
       <p>{a} plus {b} is {a + b} </p>
+      <Hello />
+      <Hello />
+      <Hello />
+      <Hello />
+      
     </div>
   )
 }
